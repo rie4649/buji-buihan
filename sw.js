ac9799160046fc6bbedf2357f47657799b4c71aa
rie4649 <okm.co@icloud.com>
2026-07-08 13:39:41 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
+// 最小限のサービスワーカー(PWAとしてインストール可能にするために必要)
+self.addEventListener("install", () => {
+self.skipWaiting();
+});
 
+self.addEventListener("activate", (event) => {
+event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+// 通常のネットワーク通信をそのまま使う(キャッシュ処理は行わない)
+});
